078d91ed40488747d8ffe87210e66e7c0f51281f
赵鑫磊 <zhaoxinlei@baikemy.com>
2018-03-14 11:17:55 +0800

    ...

--- docx2json.py
+++ docx2json.py
@@ -6,10 +6,20 @@ from docx import Document
 
 def main():
     doc = Document(docx='疾病KG卡标准模板.docx')
+    file = open('output.md', 'w', encoding='utf-8')
+    title = ''
     for para in doc.paragraphs:
-        print(para.style.name, para.style.font.size, para.text)
-        #for run in para.runs:
-        #    print(run.style.type)
+        if para.style.name == 'Heading 1':
+            file.writelines('#' + para.text.replace(' (22个字)', '') + '\n')
+            print(para.text, para.style.font.size.pt)
+        elif para.style.name == 'Heading 2':
+            file.writelines('##' + para.text.replace(' (22个字)', '') + '\n')
+            print(para.text, para.style.font.size.pt)
+        else:
+            file.writelines(para.text + '\n')
+            for run in para.runs:
+                print(run.style.font.size)
+    file.close()
 
 
 if __name__ == '__main__':
